0b29c0c5b37eb4738bd4ed4ec11522c2870da5bb
南山忆 <liguohuai@bilibili.com>
2025-09-30 18:19:15 +0800

    【fix】setting window

--- PasteDirect/Source/Setting/Controller/PasteSettingWindowController.swift
+++ PasteDirect/Source/Setting/Controller/PasteSettingWindowController.swift
@@ -12,13 +12,17 @@ class PasteSettingWindowController: NSWindowController {
     private let hostingController = NSHostingController(rootView: SettingsView()).then {
         $0.sizingOptions = [.preferredContentSize]
     }
-
+    
     init() {
-        let window = NSWindow(contentRect: .zero, styleMask: [.titled, .closable, .fullSizeContentView], backing: .buffered, defer: true)
+        let window = NSWindow(contentRect: .zero, styleMask: [.titled, .closable, .fullSizeContentView], backing: .buffered, defer: false)
         window.titleVisibility = .visible
         window.titlebarSeparatorStyle = .automatic
-        super.init(window: window)
         window.contentViewController = hostingController
+        super.init(window: window)
+        // 提前触发布局
+        hostingController.view.layoutSubtreeIfNeeded()
+        window.layoutIfNeeded()
+        showWindow(self)
     }
 
     @available(*, unavailable)
